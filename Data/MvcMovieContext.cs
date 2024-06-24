35b88a7f00fe9b2ba07563df6c0c677f2647cf5d
zekiceki <cemkasap2001@gmail.com>
2024-06-24 14:12:08 +0200

    apbd 11

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+            model.Entity<Movie>(m => m.Property(p => p.ReleaseDate).HasColumnName("ReleaseDate"));
+        }
+    }
+}
